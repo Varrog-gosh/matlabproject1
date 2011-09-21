bdffe2ce6d21d37b82c704a8d97bd914d3a9e1a2
Clément Delbouys <clement.delbouys@gmail.com>
2011-09-21 13:34:34 +0200

    I have updated data.m, we have to take a symetric matrix, and we didn't respect this property with the previous matrix H

--- data.m
+++ data.m
@@ -1,9 +1,12 @@
 function [b, H, c] = data
-%DATA generate the data for the problem with different dimension N of the matrices
-% b and c are real vectors and H a symmetric positive definit matrix
+%DATA Generate the data for the problem with different dimension N of the matrices
+% b and c are real vectors and H a symmetric positive definite matrix
+% If you want to changer the datas of the problem, you have to modify this
+% file. Be careful with the size of the matices, b abd c are column vectors
+% and H a square matrix, with the same size than b or c.
 
-b = [ 1; 0 ];
-c = [ 1; 1 ];
-H = [ 1  2 ; 0  1 ];
+b = [ 1; 0];
+c = [ 1; 1];
+H = [ 1, 2 ; 2, 1];
 
 end
