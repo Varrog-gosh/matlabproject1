aa7058ad8e194c7deb28eb8773b69db8ce90c1ff
Varrog-gosh <max@4kiesel.de>
2011-09-22 15:59:39 +0200

    Improved steepest decent methode added, but is buggy

---
+++ improved_steepest.m
@@ -0,0 +1,18 @@
+function [ X ] = improved_steepest( X )
+%STEEPEST Find a local minimum iteratively with the initial guess x0
+
+tol = 1e-6;
+maxiterations = 10000;
+
+condition = 1;
+norm_old = norm ( grad ( X ) );
+
+while condition
+	maxiterations = maxiterations - 1;
+	X = X - get_alpha ( X ) * grad ( X );
+	residual = norm ( grad ( X ) ) / norm_old;
+	condition = (maxiterations > 0) && ( residual > tol);
+end
+
+end
+
