b40fac17f349c056fe57e82e95a3a9a54fe99150
Varrog-gosh <max@4kiesel.de>
2011-09-22 18:02:27 +0200

    Steepest is now update, and there are 4 output

--- steepest.m
+++ steepest.m
@@ -1,18 +1,32 @@
-function [ X ] = steepest( X, alpha )
-%STEEPEST Find a local minimum iteratively with the initial guess x0
+function [ iterations, X, Y, Z ] = steepest( Xk, alpha )
+%STEEPEST Find a local minimum iteratively with the initial guess x0, returns 4 variables for ploting the function : 
+% iteration is a vector with the numbers of iterations, X is a vector which contains all the iterations, Y is a vector which contains all the residual values and Z is the vector wich contain all the values of G along the iterations
 
 tol = 1e-6;
 maxiterations = 1000;
-
+[~,H,~] = data;
+dim = size(H, 2);
+X = zeros(2, maxiterations);
+Y = zeros(1, maxiterations);
+Z = zeros(1, maxiterations);
 condition = 1;
-norm_old = norm ( grad ( X ) );
+norm_old = norm ( grad ( Xk) );
 
 while condition
 	maxiterations = maxiterations - 1;
-	X = X - alpha * grad ( X );
-	residual = norm ( grad ( X ) ) / norm_old;
+	Xk = Xk - alpha * grad ( Xk );
+	if dim == 2
+			X(1, 1000-maxiterations) = Xk(1);
+			X(2, 1000-maxiterations) = Xk(2);
+	end
+	residual = norm ( grad ( Xk ) ) / norm_old;
+	Y(1000-maxiterations) = residual;
+	Z(1000-maxiterations) = problem(Xk);
 	condition = (maxiterations > 0) && ( residual > tol);
 end
+X = X(1:1000-maxiterations);
+iterations = (1:1:1000-maxiterations);
+Z = Z(1:1000-maxiterations);
+Y = Y(1:1000-maxiterations);
 
 end
-
