250fc43e8f3506a44db561a9c9bb614f930bc255
Clément Delbouys <clement.delbouys@gmail.com>
2011-09-28 13:13:44 +0200

    README updated

--- newton_steepest.m
+++ newton_steepest.m
@@ -2,8 +2,8 @@ function [ min, residual ] = newton_steepest( Xk, tol, nbiterations )
 %NEWTON_STEEPEST Calculates the minumum of the function g with an efficient
 %way, which is the combination between newton and steepest method.
 
-alphaop = alphaop(Xk);
-[Xk, ~] = steepest_calcul(Xk, alphaop, tol, nbiterations);
+alpha = alphaop(Xk);
+[Xk, ~] = steepest_calcul(Xk, alpha, tol, nbiterations);
 [Xk, residual] = newton_calcul([Xk(1,end); Xk(2, end)], tol, nbiterations);
 
 min = problem([Xk(1,end); Xk(2, end)]);
