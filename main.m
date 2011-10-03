7c8c7ae926c31a032dd15045f753ea04d3d460db
Varrog-gosh <max@4kiesel.de>
2011-10-03 12:04:05 +0200

    best switch for newton/steepest choosen

--- main.m
+++ main.m
@@ -7,7 +7,7 @@ function main ()
 % set values
 X = [ 4; -1 ];
 tol = 1e-8;
-maxiterations = 20;
+maxiterations = 50;
 
 % here is a ugly implementation: if the second argument is 
 % * a positive number, the number is used as step size for the steepest decent method
@@ -17,7 +17,7 @@ maxiterations = 20;
 [ iterations1, X1, res1, value1, time1 ] = plotmethod ( X, 0.0001, tol, maxiterations );
 [ iterations2, X2, res2, value2, time2 ] = plotmethod ( X, 0, tol, maxiterations );
 [ iterations3, X3, res3, value3, time3 ] = plotmethod ( X, Inf, tol, maxiterations );
-[ iterations4, X4, res4, value4, time4 ] = plotmethod ( X, -4, tol, maxiterations );
+[ iterations4, X4, res4, value4, time4 ] = plotmethod ( X, -2, tol, maxiterations );
 
 legends = { 'Steepest decent method with alpha = 0.0001', 'Steepest decent method with optimal alpha', 'Newton method', 'Steepest decent and Newton method' };
 %plot which shows how the residual decreases with the number of iteratives
@@ -48,7 +48,7 @@ hold off;
 %plot which shows the iterations on the surface of g
 %the plots can only be performend with n = 2
 [ ~, H, ~ ] = data;
-if size ( H, 2 ) == 2
+if size ( H, 2 ) == 3
 	figure; 
 	drawfunction;
 	hold on;
@@ -60,8 +60,8 @@ if size ( H, 2 ) == 2
 	title('');
 	legend('g', legends{1}, legends{2}, legends{3}, legends{4});
 end
-time1
-time2
-time3
-time4
+time1;
+time2;
+time3;
+1000*time4
 end
